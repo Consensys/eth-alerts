305a85239a53b8a6304d82f285e6d563cc41a906
Denís Graña <denisgranha@gmail.com>
2017-03-11 18:18:35 +0100

    static files finder

--- dapp/settings/base.py
+++ dapp/settings/base.py
@@ -99,9 +99,20 @@ CACHES = {
 # STATIC FILE CONFIGURATION
 # ------------------------------------------------------------------------------
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#static-root
-STATIC_ROOT = ''
+STATIC_ROOT = str(ROOT_DIR('staticfiles'))
+
 STATIC_URL = '/static/'
-STATICFILES_DIRS = ( os.path.join('static'), )
+
+# See: https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#std:setting-STATICFILES_DIRS
+STATICFILES_DIRS = (
+    str(DJANGO_DIR.path('static')),
+)
+
+# See: https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#staticfiles-finders
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+)
 
 # GENERAL CONFIGURATION
 # ------------------------------------------------------------------------------
